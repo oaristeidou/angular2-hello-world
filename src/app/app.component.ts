a501c89eedc2874ebba8d13c6ee51732590d6e4c
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-04-04 16:19:28 +0200

    - Angular 2 Workshop

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by odyssefs on 04.04.17.
+ */
+import {Component} from '@angular/core'
+
+@Component({
+  selector:'app-hello-world',
+  templateUrl: 'app.template.html'
+})
+
+export class AppComponent {
+  title='Hello World!'
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by odyssefs on 04.04.17.
+ */
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+
+import {BrowserModule} from '@angular/platform-browser'
+
+
+@NgModule({
+  declarations: [AppComponent],
+  imports:[BrowserModule],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule{}
